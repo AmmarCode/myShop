a1e7c76900b5adc8fe80c7cd1f150bb363cfcab5
AmmarCode <ammar.code01@gmail.com>
2022-03-02 09:03:00 -0500

    Add add to cart button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+  ListGroupItem,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from 'axios';
+import Loading from "../components/Loading";
+import Message from "../components/Message";
+import { listProductDetails } from "../actions/productActions";
 
-const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState([]);
+const ProductScreen = ({ match, history }) => {
+  const [qty, setQty] = useState(1);
+
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { error, loading, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`)
-      setProduct(data)
-    }
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
-    fetchProduct()
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
 
-  }, [])
   return (
     <div>
       <Link
@@ -23,67 +38,103 @@ const ProductScreen = ({ match }) => {
         className="fas fa-home my-3"
         style={{ color: "white", textDecoration: "none" }}
       >
-        {" "}Back to homepage
+        {" "}
+        Back to homepage
       </Link>
 
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid/>
-        </Col>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
 
-        <Col md={3}>
-          <ListGroup variant="flush" style={{backgroundColor: "transparent"}}>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
+          <Col md={3}>
+            <ListGroup
+              variant="flush"
+            >
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
 
-            <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`} color={"#f8e825"} />
-            </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                  color={"#f8e825"}
+                />
+              </ListGroup.Item>
 
-            <ListGroup.Item>
-              Price: ${product.price}
-            </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
 
-            <ListGroup.Item>
-              Desc: {product.description}
-            </ListGroup.Item>
-          </ListGroup>
-
-        </Col>
-
-        <Col md={3}>
-          <Card>
-            <ListGroup variant="flush">
-              <ListGroupItem>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroupItem>
-
-              <ListGroupItem>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
-                  </Col>
-                </Row>
-              </ListGroupItem>
-
-              <ListGroupItem>
-              <div className="d-grid gap-2">
-                <button className="btn btn-primary" disabled={product.countInStock == 0} type="button">Add to cart</button>
-              </div>
-                
-              </ListGroupItem>
+              <ListGroup.Item>Desc: {product.description}</ListGroup.Item>
             </ListGroup>
-          </Card>
+          </Col>
 
-        </Col>
-      </Row>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In stock" : "Out of stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs="auto" className="my-1">
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                        {
+                          [...Array(product.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                          ))
+                        }
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
+                <ListGroup.Item>
+                  <div className="d-grid gap-2">
+                    <Button                      
+                      className="btn btn-primary"
+                      disabled={product.countInStock == 0}
+                      type="button"
+                      onClick={addToCartHandler}
+                    >
+                      Add to cart
+                    </Button>
+                  </div>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
