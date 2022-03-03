6c409a24e21cb91d0a9c2ac43e0ec23bd5c8bd66
AmmarCode <ammar.code01@gmail.com>
2022-03-03 16:20:18 -0500

    Add register user and user profile urls

--- backend/shop_api/urls.py
+++ backend/shop_api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name='routes'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register_user'),
+    path('users/profile/', views.getUserProfile, name='user_profile'),
+    path('users/', views.getUsers, name='users'),
+
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product'),
 ]
