c0f1dc35213f996b4d96fc37c20e3057c8ae8a61
AmmarCode <ammar.code01@gmail.com>
2022-03-04 18:04:19 -0500

    Add user profile update url

--- backend/shop_api/urls.py
+++ backend/shop_api/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('users/register/', views.registerUser, name='register_user'),
     path('users/profile/', views.getUserProfile, name='user_profile'),
+    path('users/profile/update/', views.updateUserProfile,
+         name='update_user_profile'),
     path('users/', views.getUsers, name='users'),
 
     path('products/', views.getProducts, name='products'),
