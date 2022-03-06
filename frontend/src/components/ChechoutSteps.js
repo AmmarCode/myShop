03351b34d92115617c3d202c4b56b1a31fc67f04
AmmarCode <ammar.code01@gmail.com>
2022-03-06 08:37:05 -0500

    Initial commit chekout steps

---
+++ frontend/src/components/ChechoutSteps.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const ChechoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+		<Nav className="justify-content-center mb-4">
+		
+			<Nav.Item>
+				{step1 ? (
+					<LinkContainer style={{ color:"white" }} to='/login'>
+						<Nav.Link>Login</Nav.Link>
+					</LinkContainer>
+				) : (
+					<Nav.Link disabled>Login</Nav.Link>
+				)}
+			</Nav.Item>
+
+			<Nav.Item>
+				{step2 ? (
+					<LinkContainer style={{ color:"white" }} to='/shipping'>
+						<Nav.Link>Shipping</Nav.Link>
+					</LinkContainer>
+				) : (
+					<Nav.Link disabled>Shipping</Nav.Link>
+				)}
+			</Nav.Item>
+
+			<Nav.Item>
+				{step3 ? (
+					<LinkContainer style={{ color:"white" }} to='/payment'>
+						<Nav.Link>Payment</Nav.Link>
+					</LinkContainer>
+				) : (
+					<Nav.Link disabled>Payment</Nav.Link>
+				)}
+			</Nav.Item>
+
+			<Nav.Item>
+				{step4 ? (
+					<LinkContainer style={{ color:"white" }} to='/placeorder'>
+						<Nav.Link>Place Order</Nav.Link>
+					</LinkContainer>
+				) : (
+					<Nav.Link disabled>Place Order</Nav.Link>
+				)}
+			</Nav.Item>
+
+		</Nav>
+	)
+};
+
+export default ChechoutSteps;
