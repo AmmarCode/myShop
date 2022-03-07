be6f32571aad019846be9bd260b810afaa9cef33
AmmarCode <ammar.code01@gmail.com>
2022-03-06 22:58:59 -0500

    Remove unused code

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
-import ChechoutSteps from "../components/ChechoutSteps";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 
 const ShippingScreen = ({ history }) => {
@@ -13,7 +13,6 @@ const ShippingScreen = ({ history }) => {
 
   const [address, setAddress] = useState(shippingAddress.address);
   const [city, setCity] = useState(shippingAddress.city);
-  const [stateName, setStateName] = useState(shippingAddress.stateName);
   const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
   const [country, setCountry] = useState(shippingAddress.country);
 
@@ -23,7 +22,6 @@ const ShippingScreen = ({ history }) => {
       saveShippingAddress({
         address,
         city,
-        stateName,
         postalCode,
         country,
       })
@@ -33,7 +31,7 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
-      <ChechoutSteps step1 step2 />
+      <CheckoutSteps step1 step2 />
       <h2>Shipping</h2>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
@@ -55,17 +53,6 @@ const ShippingScreen = ({ history }) => {
             placeholder="Enter City"
             value={city ? city : ""}
             onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId="stateName">
-          <Form.Label>State</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Enter State"
-            value={stateName ? stateName : ""}
-            onChange={(e) => setStateName(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
