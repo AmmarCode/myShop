585af5dc81f5ad465dd008d358cabeaf449ed51c
AmmarCode <ammar.code01@gmail.com>
2022-03-06 22:54:07 -0500

    Fix typo

--- backend/shop_api/models.py
+++ backend/shop_api/models.py
@@ -69,7 +69,7 @@ class ShippingAddress(models.Model):
     city = models.CharField(max_length=200, null=True, blank=True)
     postlCode = models.CharField(max_length=200, null=True, blank=True)
     country = models.CharField(max_length=200, null=True, blank=True)
-    shipingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
