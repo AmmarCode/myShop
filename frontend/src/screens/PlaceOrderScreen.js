86de58136fd100fdf3db9b56c69f18cf9f7758f7
AmmarCode <ammar.code01@gmail.com>
2022-03-10 22:45:50 -0500

    Fix bug(remove order._id from useEffect dependenc)

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -29,7 +29,7 @@ const PlaceOrderScreen = ({ history }) => {
 			history.push(`/order/${order._id}`)
 			dispatch({type: ORDER_CREATE_RESET})
 		}
-	}, [dispatch, success, history, order._id])
+	}, [dispatch, success, history])
 
 	const placeOrder = () => {
 		dispatch(createOrder({
