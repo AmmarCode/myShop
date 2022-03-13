644bbc458ead0a8a7fe6600a8ffa1c641479f888
AmmarCode <ammar.code01@gmail.com>
2022-03-13 17:47:20 -0400

    Add user edit, product list, product edit screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,9 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
-import UserEditScreen from './screens/UserEditScreen'
-
+import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 const App = () => {
   return (
@@ -35,7 +36,10 @@ const App = () => {
           <Route path="/cart/:id?" component={CartScreen} />
 
           <Route path="/admin/userlist" component={UserListScreen} />
-          <Route path='/admin/user/:id/edit' component={UserEditScreen} />        
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+
+          <Route path="/admin/productlist" component={ProductListScreen} />
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
         </Container>
       </main>
       <Footer />
