0efa8fbb64b3ddbda4fe0ea03fcb836e71320e34
AmmarCode <ammar.code01@gmail.com>
2022-03-14 12:07:39 -0400

    Add paginate, carousel

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,34 +4,50 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import Loading from '../components/Loading';
 import Message from '../components/Message';
+import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 import { listProducts } from "../actions/productActions";
 
-function HomeScreen() {
+function HomeScreen({ history }) {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
-  const { error, loading, products } = productList;
+  const { error, loading, products, page, pages } = productList;
 
+  let keyword = history.location.search
+
+  console.log(keyword)
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
     <div>
+      
+
+      {!keyword && 
+      <div>
+        <h1>Top Rated Products</h1>
+        <ProductCarousel />
+      </div>}
+
       <h1>Latest Products</h1>
       {loading ? (
         <Loading />
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products.map((product) => {
-            return (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            );
-          })}
-        </Row>
+        <div>
+          <Row>
+            {products.map((product) => {
+              return (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              );
+            })}
+          </Row>
+          <Paginate page={page} pages={pages} keyword={keyword} />
+        </div>
       )}
     </div>
   );
