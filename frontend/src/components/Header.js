4f5b7b463f66d654b44baae84d41d61d213bbdcb
AmmarCode <ammar.code01@gmail.com>
2022-03-25 08:05:02 -0400

    Turn search auto complete off

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -53,6 +53,7 @@ const Header = () => {
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
+                autoComplete="off"
               />
               <Button 
               type='submit'
